635aa5f5347ed40f9eb86d28b50e119d5643f5b5
Andolo2 <andreasolofsson123@gmail.com>
2022-04-18 20:12:35 +0200

    Website is done

---
+++ bookmarks.js
@@ -0,0 +1,43 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+    {
+      title: "Daily",
+      links: [
+        { name: "Inbox", url: "https://mail.google.com" },
+        { name: "GitHub", url: "https://github.com" },
+        { name: "Drive", url: "https://drive.google.com" },
+      ],
+    },
+    {
+      title: "Media",
+      links: [
+        { name: "Youtube", url: "https://youtube.com" },
+        { name: "Netflix", url: "https://netflix.com" },
+        { name: "Omni", url: "https://omni.se/" },
+        
+      ],
+    },
+    {
+      title: "Reddit",
+      links: [
+        { name: "Reddit Start Page", url: "https://reddit.com/" },,
+        
+        
+        { name: "/r/Sweden", url: "https://www.reddit.com/r/sweden/" },
+        {
+          name: "/r/OnePiece",
+          url: "https://www.reddit.com/r/OnePiece/",
+        },
+        { name: "/r/learnprogramming/", url: "reddit.com/r/learnprogramming/" },
+        
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        { name: "Twitter", url: "https://twitter.com" },
+        { name: "Linkedin", url: "https://www.linkedin.com/feed/" },
+      ],
+    },
+  ];
+  
